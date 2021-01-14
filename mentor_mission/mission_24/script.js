0f469db11da4ae8c076cb70d4aafe063598b10bc
chrislinlin <asx135@hotmail.com>
2021-01-14 23:01:33 +0800

    when i add item clear the input

--- mentor_mission/mission_24/script.js
+++ mentor_mission/mission_24/script.js
@@ -11,4 +11,10 @@ function addItem(name, amount){
 
     item_str = '<li class ="plus">'+name+'<span>'+amount+'</span>'+'<button class = "delete-btn">x</button>';
     $('#list').append(item_str)
+    clearForm();
+}
+
+//clear transaction
+function clearForm(){
+    $('#form').find("input").val("");
 }
