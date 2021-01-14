54712d272246908aac1d057fb0853af3cf1451af
chrislinlin <asx135@hotmail.com>
2021-01-14 22:54:51 +0800

    add item in list(layout)

---
+++ mentor_mission/mission_24/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#form").find('button').click(function(e){
+        e.preventDefault();
+        console.log('good');
+        addItem(123,123);
+    })
+})
+
+//add item in history
+function addItem(name, amount){
+
+    item_str = '<li class ="plus">'+name+'<span>'+amount+'</span>'+'<button class = "delete-btn">x</button>';
+    $('#list').append(item_str)
+}
