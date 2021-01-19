1da656b203278e74aba47f242f12df6571f70ad6
chrislinlin <asx135@hotmail.com>
2021-01-19 12:32:50 +0800

    finished pauseSong and switch the icon

--- mission_25/script.js
+++ mission_25/script.js
@@ -10,8 +10,32 @@ const progressContainer = $('#progress-container')
 // Song titles
 const songs = ['hey', 'summer', 'ukulele'];
 
-//play song
-playBtn.click(function playSong(){
-    audio.play()
+//event click
+playBtn.click(function(){
+    const isPlaying = musicContainer.hasClass('play');
+    // console.log(isPlaying);
+    // console.log(musicContainer)
+    if(isPlaying){
+        pauseSong();
+    }else{
+        playSong();
+    }
 });
 
+//play song
+function playSong(){
+    musicContainer.addClass('play')
+    playBtn.find('i.fas').removeClass('fa-play');
+    playBtn.find('i.fas').addClass('fa-pause');
+    audio.play();
+}
+
+
+//pause song
+function pauseSong(){
+    musicContainer.removeClass('play')
+    playBtn.find('i.fas').removeClass('fa-pause');
+    playBtn.find('i.fas').addClass('fa-play');
+    audio.pause();
+    
+}
