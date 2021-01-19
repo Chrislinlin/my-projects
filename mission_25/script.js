48b2f75a16a4294af0cca5f2f1fc394b19f73b29
chrislinlin <asx135@hotmail.com>
2021-01-19 11:56:52 +0800

    finished play function

--- mission_25/script.js
+++ mission_25/script.js
@@ -6,3 +6,12 @@ const audio = $('#audio')[0];
 const cover = $('#cover')[0];
 const title = $('#title')
 const progressContainer = $('#progress-container')
+
+// Song titles
+const songs = ['hey', 'summer', 'ukulele'];
+
+//play song
+playBtn.click(function playSong(){
+    audio.play()
+});
+
