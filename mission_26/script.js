403e6489c3698694e13b9fde3531e34acf2b8cb6
chrislinlin <asx135@hotmail.com>
2021-01-21 11:28:32 +0800

    set the loader disappear in 1s

--- mission_26/script.js
+++ mission_26/script.js
@@ -1,37 +1,65 @@
 const filterContainer = $('.filter-container');
 const filter =$('#filter');
-const loading =$('.loador');
+const loading =$('.loader');
+const postContainer = $('.post-container')
 
 let limit = 5;
 let page =1;
 
 //使用$.ajax()
-$.ajax({
-    url: `https://jsonplaceholder.typicode.com/posts?
-    _limit=${limit}&_page=${page}`,
-    method:'get',
-    dataType:'json',
-    // data:
-    success:function(data){
-
-        console.log(data)
-
-        // posts.forEach(post => {
-        //     const postEl = document.createElement('div');
-        //     postEl.classList.add('post');
+function showPost(){
+    $.ajax({
+        url: `https://jsonplaceholder.typicode.com/posts?
+        _limit=${limit}&_page=${page}`,
+        method:'get',
+        dataType:'json',
+        success:function(data){
+            //使用forEach，遍及每筆資料，
+            
+            // console.log(data)
+            data.forEach(function(post){
+                const postEl = $('<div></div>').addClass('post');
+                postEl.html(`
+                <div class="number">${post.id}</div>
+                <div class="post-info">
+                    <h2 class ="post-title">${post.title}</h2>
+                    <p class = "post-body">${post.body}></p>
+                </div>
+                `);
+                postContainer.append(postEl);
+                
+               
+            })
     
-        //     postEl.innerHTML = `
-        //     <div class="number">${post.id}</div>
-        //     <div class="post-info">
-        //         <h2 class ="post-title">${post.title}</h2>
-        //         <p class = "post-body">${post.body}></p>
-        //     </div>
-        //     `;
-        //     postsContainer.appendChild(postEl);
-    
-        // });
+            // posts.forEach(post => {
+            //     const postEl = document.createElement('div');
+            //     postEl.classList.add('post');
+        
+            //     postEl.innerHTML = `
+            //     <div class="number">${post.id}</div>
+            //     <div class="post-info">
+            //         <h2 class ="post-title">${post.title}</h2>
+            //         <p class = "post-body">${post.body}></p>
+            //     </div>
+            //     `;
+            //     postsContainer.appendChild(postEl);
+        
+            // });
+        }
+    })
+}
+    //showLoading()
+function showLoading(){
+    loading.addClass('show');
+    setTimeout(function(){
+        loading.removeClass('show')
+    },1000);
+    }
+showPost();
+
+$(window).scroll(function(){
+    if ($(document).scrollTop() + $(window).height() > $(document).height() - 5) {
+        showLoading();
     }
 })
-    // .done(function (picsumImages) {
-    //     // Handling the images here by using picsumImages
-    // });
+
