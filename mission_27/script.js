9e47bee53980bbb546c3723ceae19a1467ad4850
chrislinlin <asx135@hotmail.com>
2021-01-22 16:25:39 +0800

    finished get random word by word API

--- mission_27/script.js
+++ mission_27/script.js
@@ -31,8 +31,32 @@ let score;
 //init time
 let time = 10;
 
-//get random word
-function getRandomWord(){
-    return words[Math.floor(Math.random()* words.length)]
+// //get random word 
+// function getRandomWord(){
+//     return words[Math.floor(Math.random()* words.length)]
+// }
+// // console.log(getRandomWord())
+
+//get word by API
+RandomWord()
+function RandomWord() {
+
+
+    $.ajax({
+        method: "get",
+        url: 'https://random-word-api.herokuapp.com/word?number=1',
+        dataType: "json",
+        success: function (data) {
+            $('h1').text(data);
+          }  
+    });
 }
-console.log(getRandomWord())
+
+
+
+
+//add word to DOM
+function addWordToDOM(){
+    randomWord = getRandomWord()
+    $('#word')
+}
