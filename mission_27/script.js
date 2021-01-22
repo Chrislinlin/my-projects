220b2ea5f3c396f56ecc7847de7ae9819cd44e6b
chrislinlin <asx135@hotmail.com>
2021-01-22 13:22:56 +0800

    add the word list and init word/time/score

---
+++ mission_27/script.js
@@ -0,0 +1,23 @@
+//list of words for game
+const words= [
+    'sigh',
+    'tense',
+    'airplane',
+    'ball',
+    'pies',
+    'juice',
+    'warlike',
+    'bad',
+    'north',
+    'dependent',
+    'steer',
+    'silver',
+    'hoghfalutin',
+    'superficial',
+    'quince',
+    'eight',
+    'feeble',
+    'admit',
+    'drag',
+    'loving'
+];
