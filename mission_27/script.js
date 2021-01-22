36e360bae09336c505e585e2ebb012a0d57b58db
chrislinlin <asx135@hotmail.com>
2021-01-22 14:32:30 +0800

    create randomWord function

--- mission_27/script.js
+++ mission_27/script.js
@@ -21,3 +21,18 @@ const words= [
     'drag',
     'loving'
 ];
+
+//init word
+let randomWord;
+
+//init score
+let score;
+
+//init time
+let time = 10;
+
+//get random word
+function getRandomWord(){
+    return words[Math.floor(Math.random()* words.length)]
+}
+console.log(getRandomWord())
