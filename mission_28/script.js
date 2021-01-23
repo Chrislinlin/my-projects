c9dd436f412ae87d0b818c6d163e74242f6364fa
chrislinlin <asx135@hotmail.com>
2021-01-23 16:57:37 +0800

    add speechRecognition and create object

--- mission_28/script.js
+++ mission_28/script.js
@@ -2,7 +2,14 @@ $('#msg')
 
 const randomNum = getRandomNum();
 
-console.log(getRandomNum());
+console.log("number: ",randomNum);
 function getRandomNum(){
     return Math.floor(Math.random()*100) +1;
 }
+
+//speechRecognition
+window.SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+
+//create object
+let recognition = new window.SpeechRecognition();
