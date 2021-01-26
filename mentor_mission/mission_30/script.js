c5bc4430a29c1ba8c58965c6211c180637eee4ff
chrislinlin <asx135@hotmail.com>
2021-01-26 23:15:12 +0800

    creat video arr and let them into DOM

---
+++ mentor_mission/mission_30/script.js
@@ -0,0 +1,41 @@
+const draggableList =$('#draggable-list')[0]
+
+const hottestVideo = [
+    '羅一鈞副組長',
+    '蔡阿嘎與孕婦二伯',
+    '粿粿-花蓮落下事件',
+    '這群人-請假經典語錄',
+    '黃氏兄弟-我想跟家人說',
+    '中指通的歐美女優精選',
+    '孫生去監獄看媽媽',
+    '狠愛演-謝謝大家',
+    '反正我很閒-卑鄙源之助的一天',
+    '東森51-臨終前最後26秒奮力道別'
+]
+const listItems= [];
+
+let dragStartIndex;
+creatList()
+
+
+//creat List items into DOM
+function creatList(){
+    [...hottestVideo]
+    .forEach(function(video,index) {
+        const listItem = document.createElement('li');
+        //index =0~9
+        listItem.setAttribute('data-index', index);
+        listItem.innerHTML = `
+        <span class ='number'>${ index + 1 }</span>
+        <div class ="draggable" draggable ="true">
+            <p class ="video-name">${video}</p>
+            <i class = "fas fa-grip-lines></i>
+        </div>
+        `;
+        listItems.push(listItem);
+        draggableList.appendChild(listItem);
+        
+    });
+
+
+}
