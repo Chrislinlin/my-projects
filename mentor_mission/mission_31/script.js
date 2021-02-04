deac86b29a238eb75aa5fcdea4d51becdb8514f2
chrislinlin <asx135@hotmail.com>
2021-02-05 06:48:33 +0800

    create getRepoData function

--- mentor_mission/mission_31/script.js
+++ mentor_mission/mission_31/script.js
@@ -10,14 +10,28 @@ function getUserData(){
         method:'GET',
         datatype:'json',
         success: function(data){
-            
             console.log(data);
+
         },
         error: function(){
             console.log('ohhno')
         }
     })
 }
+function getRepoData(){
+    $.ajax({
+        url:`https://api.github.com/users/${userName}/repos?per_page=5&sort=created: asc`,
+        method:'GET',
+        datatype:'json',
+        success: function(data55){
+            console.log(data55)
+        },
+        error: function(){
+            console.log('ohhno again!')
+        }
+    })
+}
 searchBtn.click(function(){
-    getUserData()
+    getUserData();
+    getRepoData();
 })
