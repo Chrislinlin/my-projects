07f569732698c57597fb89e4d21929cedf04c398
chrislinlin <asx135@hotmail.com>
2021-02-14 10:11:31 +0800

    finished selectCity part

--- mentor_mission/mission_32/script.js
+++ mentor_mission/mission_32/script.js
@@ -1,4 +1,6 @@
 let data;
+let city;
+
 $.ajax({
     url:'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-373C6328-6BF2-41B3-BB3B-147802B82875&format=JSON&locationName=&elementName=&sort=time',
     method: "GET",
@@ -6,8 +8,32 @@ $.ajax({
     success: function(res){
         data = res.records.locations[0];
         // console.log(data);
-        let city = data.location;
+        city = data.location;
         // console.log(city);
         todayWeather();
+        selectCity(city);
     }
 })
+function todayWeather(){
+    // console.log(123)
+    $('#weatherNow').html('');
+    
+}
+function selectCity(data){
+    const $ = document.querySelector.bind(document);
+    let select = $('select');
+    
+    for(let i =0;i< data.length;i++){
+        city = data[i].locationName;
+        let value =i;
+        select.innerHTML += `
+        <option value="${value}">${city}</option>
+    `;
+    }
+    console.log(city);
+
+}
+$('#select').click(function() {
+    console.log(123)
+    cityIndex = $('#select :selected').val()
+});
