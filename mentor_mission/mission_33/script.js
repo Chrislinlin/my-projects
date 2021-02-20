000e4c79f13c7f9a4763f568d87310c2dfd14793
chrislinlin <asx135@hotmail.com>
2021-02-20 18:38:30 +0800

    finished the nextPage function

--- mentor_mission/mission_33/script.js
+++ mentor_mission/mission_33/script.js
@@ -1,29 +1,30 @@
 
-$(document).ready(function(){
     let API_Key = 'AIzaSyCSwkueGwGH8xdWDDgcMMAdXDRxKg0uEiE';
     let video ='';
-    let videos = $('#videos')
+    let videos = $('#videos');
+    let q;
     $('#form').submit(function(event){
         event.preventDefault();
 
         let search = $('#search').val();
         vedioSearch(API_Key, search, 10);
     })
-})
+
 
 //search function
 
 
-function vedioSearch(key, search, maxResult){
+function vedioSearch(key, q, maxResult){
     $('#videos').html('');
+    $('#buttons').html('');
+
+     q = $('#search').val();
     $.get("https://www.googleapis.com/youtube/v3/search",{
         key: 'AIzaSyCSwkueGwGH8xdWDDgcMMAdXDRxKg0uEiE',
         type: 'video',
         part: 'snippet, id',
         maxResults: maxResult,
-        q: search,
-        
-        
+        q: q,
     }, 
     function(data){
         let nextPageToken = data.nextPageToken;
@@ -39,17 +40,50 @@ function vedioSearch(key, search, maxResult){
         let buttons =getButton(nextPageToken, prevPageToken);
         $('#button').append(buttons);
     })
-
-
-
 }
+
+
 //create the button
-function getButton(prevPageToken, nextPageToken){
-    let btnOutput = '<div class ="button-container">' + 
-    '<button id = "next-button" class = "paging-button" data-token= "'
-     +nextPageToken+'"  onclick = nextPage();">next page</button>';
-    console.log(btnOutput);
+function getButton(nextPageToken){
+    let q;
+    var btnOutput = '<div class="button-container">' + '<button id="next-button" class="paging-button" data-token="' +nextPageToken+'" data-query="'+q+'"+ onclick="nextPage();">Next Page</button></div>'; 
+
     return btnOutput;
+}
+
+//nextpage button
+function nextPage(){
+
+    let token = $('#next-button').data('token');
+    console.log(token)
+    let q = $('#next-button').data('query');
+
+    $('#videos').html('');
+    $('#button').html('');
+
+    q = $("#search").val();
+
+    $.get("https://www.googleapis.com/youtube/v3/search",{
+        key: 'AIzaSyCSwkueGwGH8xdWDDgcMMAdXDRxKg0uEiE',
+        type: 'video',
+        part: 'snippet, id',
+        pageToken: token,
+        q: q,
+    }, 
+    function(data){
+        let nextPageToken = data.nextPageToken;
+        let prevPageToken = data.prevPageToken;
+        console.log(data);
+        
+        data.items.forEach(item => {
+            video = getOutput(item);
+            $("#videos").append(video);
+        });
+
+        //show button
+        let buttons =getButton(nextPageToken, prevPageToken);
+        $('#button').append(buttons);
+    })
 }
 //output
 function getOutput(item){
