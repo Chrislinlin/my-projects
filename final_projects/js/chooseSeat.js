a96f68025a52a1e22beba9d5815030e2171ba49b
Chrislinlin <asx135@hotmail.com>
2021-05-20 09:22:47 +0800

    choose seat page link modify

--- final_projects/js/chooseSeat.js
+++ final_projects/js/chooseSeat.js
@@ -1,16 +1,4 @@
 
-const rows = document.querySelector('.row');
-const container = document.getElementById('container')
-console.log('sss')
-const seatSelected = document.getElementById('seat-select')
-
-container.addEventListener('click',function (e) {
-    if(e.target.classList.contains('seat')){
-        console.log('aaa')
-    e.target.classList.toggle('selected')
-    // updateSelectCount()
-}
-})
 
 // function aaa() {
 //     console.log('ssss')
--- final_projects/js/chooseTickets.js
+++ final_projects/js/chooseTickets.js
@@ -16,7 +16,107 @@ let vm= new Vue({
 
             },
         ],
-        selTotal:0
+        sum: 0,
+        totalSeats:[
+            {
+                name:"第一排",
+                list: [
+                    {
+                        num:"一號",
+                },
+                    {
+                        num:"二號",
+                },
+                    {
+                        num:"三號",
+                },
+                    {
+                        num:"四號",
+                },
+                    {
+                        num:"五號",
+                },
+                    {
+                        num:"六號",
+                },
+                    {
+                        num:"七號",
+                },
+                    {
+                        num:"八號",
+                },
+                    {
+                        num:"九號",
+                },
+            ]
+            },
+            {
+                name:"第二排",
+                list: [
+                    {
+                        num:"一號",
+                },
+                    {
+                        num:"二號",
+                },
+                    {
+                        num:"三號",
+                },
+                    {
+                        num:"四號",
+                },
+                    {
+                        num:"五號",
+                },
+                    {
+                        num:"六號",
+                },
+                    {
+                        num:"七號",
+                },
+                    {
+                        num:"八號",
+                },
+                    {
+                        num:"九號",
+                },
+            ]
+            },
+            {
+                name:"第三排",
+                list: [
+                    {
+                        num:"一號",
+                },
+                    {
+                        num:"二號",
+                },
+                    {
+                        num:"三號",
+                },
+                    {
+                        num:"四號",
+                },
+                    {
+                        num:"五號",
+                },
+                    {
+                        num:"六號",
+                },
+                    {
+                        num:"七號",
+                },
+                    {
+                        num:"八號",
+                },
+                    {
+                        num:"九號",
+                },
+            ]
+            },
+        ],
+        selSeats:[],
+        test: false
 
     },
     computed:{
@@ -48,14 +148,56 @@ let vm= new Vue({
             this.tickets[index].num++
         },
         increase(){
-            const selectSeats = document.querySelectorAll('.selected')
-            console.log(selectSeats)
-            selectSeatsLength = selectSeats.length;
-            console.log(selectSeatsLength)
-            if(selectSeatsLength>0){
-                this.selTotal =selectSeatsLength
-            }
 
+// const rows = document.querySelector('.row');
+// const container = document.getElementById('container')
+// console.log('sss')
+// const seatSelected = document.getElementById('seat-select')
+
+// container.addEventListener('click',function (e) {
+//     if(e.target.classList.contains('selected')){
+//         console.log('aaa')
+//         this.sum = this.sum-1;
+//     }else{
+//         this.sum =this.sum+1;
+//     }
+//     e.target.classList.toggle('selected')
+//     console.log(e.target.classList[0]);
+    
+    
+//     // updateSelectCount()
+// })
+//             const selectSeats = document.querySelectorAll('.selected')
+//             console.log(selectSeats)
+//             selectSeatsLength = selectSeats.length;
+//             console.log(selectSeatsLength)
+//             if(selectSeatsLength>0){
+//                 this.selTotal =selectSeatsLength
+//             }
+
+//         }
+//     }
+    }, 
+    increaseSeat(i,j){
+        if(this.selSeats.length >0){
+            // console.log(i.name, j.num);
+            if(this.selSeats.indexOf(j.num)>-1){
+                this.selSeats.splice(this.selSeats.indexOf(j.num),1)
+                this.test =false;
+            }else{
+                this.selSeats.push(j.num)
+                this.test = true;
+            }
+        }else{
+            this.selSeats.push(j.num)
+            this.test = true;
+           
         }
-    }
+        console.log(this.selSeats)
+        // selSeats.push(`${i.name}-${j.num}`)
+        
+
+
+    } 
+}
 })
